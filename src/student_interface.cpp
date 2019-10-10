26c12ab22151c93d34181b179da3069647a1fc75
simonedegiacomi <simonedegiacomi97@gmail.com>
2019-10-10 10:15:09 +0200

    copy implementation of unwrap

--- src/student_interface.cpp
+++ src/student_interface.cpp
@@ -43,16 +43,29 @@ namespace student {
 
     }
 
-    void findPlaneTransform(const cv::Mat &cam_matrix, const cv::Mat &rvec, const cv::Mat &tvec,
-                            const std::vector <cv::Point3f> &object_points_plane, cv::Mat &plane_transf,
-                            const std::string &config_folder) {
-        throw std::logic_error("STUDENT FUNCTION - FIND PLANE TRANSFORM - NOT IMPLEMENTED");
+    //-------------------------------------------------------------------------
+    //          FIND PLANE TRANSFORM
+    //-------------------------------------------------------------------------
+    void findPlaneTransform(const cv::Mat& cam_matrix, const cv::Mat& rvec,
+                            const cv::Mat& tvec,
+                            const std::vector<cv::Point3f>& object_points_plane,
+                            const std::vector<cv::Point2f>& dest_image_points_plane,
+                            cv::Mat& plane_transf, const std::string& config_folder){
+
+        cv::Mat image_points;
+
+        // project points
+        cv::projectPoints(object_points_plane, rvec, tvec, cam_matrix, cv::Mat(), image_points);
+
+        plane_transf = cv::getPerspectiveTransform(image_points, dest_image_points_plane);
     }
 
-
-    void unwarp(const cv::Mat &img_in, cv::Mat &img_out, const cv::Mat &transf, const double scale,
-                const std::string &config_folder) {
-        throw std::logic_error("STUDENT FUNCTION - UNWRAP - NOT IMPLEMENTED");
+    //-------------------------------------------------------------------------
+    //          UNWARP TRANSFORM
+    //-------------------------------------------------------------------------
+    void unwarp(const cv::Mat& img_in, cv::Mat& img_out, const cv::Mat& transf,
+                const std::string& config_folder){
+        cv::warpPerspective(img_in, img_out, transf, img_in.size());
     }
 
     bool processMap(const cv::Mat &img_in, const double scale, std::vector <Polygon> &obstacle_list,
