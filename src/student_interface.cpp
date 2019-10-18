0282113fc611cd2beda47a6a4cd37de1f17222d4
tomdan95 <danny.tomasi.td@gmail.com>
2019-10-18 12:49:00 +0200

    added loadimmage prof implementation

--- src/student_interface.cpp
+++ src/student_interface.cpp
@@ -6,9 +6,44 @@
 
 namespace student {
 
-    void loadImage(cv::Mat &img_out, const std::string &config_folder) {
-        throw std::logic_error("STUDENT FUNCTION - LOAD IMAGE - NOT IMPLEMENTED");
-    }
+    void loadImage(cv::Mat& img_out, const std::string& config_folder){  
+        static bool initialized = false;
+        static std::vector<cv::String> img_list; // list of images to load
+        static size_t idx = 0;  // idx of the current img
+        static size_t function_call_counter = 0;  // idx of the current img
+        const static size_t freeze_img_n_step = 30; // hold the current image for n iteration
+        static cv::Mat current_img; // store the image for a period, avoid to load it from file every time
+        
+        if(!initialized){
+            const bool recursive = false;
+            // Load the list of jpg image contained in the config_folder/img_to_load/
+            cv::glob(config_folder + "/img_to_load/*.jpg", img_list, recursive);
+            
+            if(img_list.size() > 0){
+            initialized = true;
+            idx = 0;
+            current_img = cv::imread(img_list[idx]);
+            function_call_counter = 0;
+            }else{
+            initialized = false;
+            }
+        }
+        
+        if(!initialized){
+            throw std::logic_error( "Load Image can not find any jpg image in: " +  config_folder + "/img_to_load/");
+            return;
+        }
+        
+        img_out = current_img;
+        function_call_counter++;  
+        
+        // If the function is called more than N times load increment image idx
+        if(function_call_counter > freeze_img_n_step){
+            function_call_counter = 0;
+            idx = (idx + 1)%img_list.size();    
+            current_img = cv::imread(img_list[idx]);
+        }
+}
 
     int imageCounter = 0;
 
