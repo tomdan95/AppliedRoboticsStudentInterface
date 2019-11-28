a0953eb0aa810dd6b1f101b6cc91ec8dffa61efe
simonedegiacomi <simonedegiacomi97@gmail.com>
2019-11-28 09:48:37 +0100

    update 'findRobot' implementation (copy from professor_interface)

--- src/demos/find_robot.cpp
+++ src/demos/find_robot.cpp
@@ -1,9 +1,14 @@
+#define FIND_ROBOT_DEBUG_PLOT
 #include "../pipeline/detection/find_robot.hpp"
 
-cv::Mat loadAndShowImage(string fileName);
+
+
+cv::Mat loadImage(string fileName);
+void showImageAndWaitKeyPress(cv::Mat &image);
 
 int main() {
-    cv::Mat img = loadAndShowImage("/home/lar2019/robot/AppliedRoboticsStudentInterface/src/areana_samples/000.jpg");
+    cv::Mat img = loadImage("/home/lar2019/robot/AppliedRoboticsStudentInterface/src/areana_samples/000.jpg");
+    //showImageAndWaitKeyPress(img);
     Polygon triangle;
     double baricenterX, baricenterY;
     double theta;
@@ -21,15 +26,19 @@ int main() {
     return 0;
 }
 
-cv::Mat loadAndShowImage(string fileName) {
+cv::Mat loadImage(string fileName) {
     cv::Mat image = cv::imread(fileName, CV_LOAD_IMAGE_COLOR);
     if (!image.data) {
         cout << "Could not open or find the image: " << fileName << endl;
         cout << "(If you're using a relative file path, check from which folder you're running this executable)" << endl;
         exit(-1);
     }
+    return image;
+}
+
+
+void showImageAndWaitKeyPress(cv::Mat &image) {
     namedWindow("Loaded image", cv::WINDOW_AUTOSIZE);// Create a window for display.
     imshow("Display window", image);
     cv::waitKey(0);
-    return image;
 }
--- src/pipeline/detection/find_robot.cpp
+++ src/pipeline/detection/find_robot.cpp
@@ -7,8 +7,110 @@
 using namespace std;
 
 namespace student {
+//-------------------------------------------------------------------------
+    //          FIND ROBOT
+    //-------------------------------------------------------------------------
+    bool processRobot(const cv::Mat& hsv_img, const double scale, Polygon& triangle, double& x, double& y, double& theta){
 
-/*!
+        cv::Mat blue_mask;
+
+        cv::inRange(hsv_img, cv::Scalar(90, 50, 50), cv::Scalar(140, 255, 255), blue_mask);
+
+        // Process blue mask
+        std::vector<std::vector<cv::Point>> contours, contours_approx;
+        std::vector<cv::Point> approx_curve;
+        cv::findContours(blue_mask, contours, cv::RETR_EXTERNAL, cv::CHAIN_APPROX_SIMPLE);
+
+        // cv::imshow("filterrrr", blue_mask);
+        // cv::waitKey(1);
+
+        // cv::Mat contours_img;
+        // contours_img = hsv_img.clone();
+
+        // drawContours(contours_img, contours, -1, cv::Scalar(0,0,0), 4, cv::LINE_AA);
+        // std::cout << "N. contours: " << contours.size() << std::endl;
+
+
+        bool found = false;
+        for (int i=0; i<contours.size(); ++i)
+        {
+            //std::cout << (i+1) << ") Contour size: " << contours[i].size() << std::endl;
+
+            cv::approxPolyDP(contours[i], approx_curve, 10, true);
+            contours_approx = {approx_curve};
+
+            // cv::drawContours(contours_img, contours_approx, -1, cv::Scalar(0,170,220), 3, cv::LINE_AA);
+
+            double area = cv::contourArea(approx_curve);
+
+            if (approx_curve.size() != 3) continue;
+
+            if (area < 300 || area>3000) continue;
+
+
+            found = true;
+            break;
+        }
+
+        if (found)
+        {
+            for (const auto& pt: approx_curve) {
+                triangle.emplace_back(pt.x/scale, pt.y/scale);
+            }
+
+            double cx = 0, cy = 0;
+            for (auto item: triangle)
+            {
+                cx += item.x;
+                cy += item.y;
+            }
+            cx /= triangle.size();
+            cy /= triangle.size();
+
+            double dst = 0;
+            Point vertex;
+            for (auto& item: triangle)
+            {
+                double dx = item.x-cx;
+                double dy = item.y-cy;
+                double curr_d = dx*dx + dy*dy;
+                if (curr_d > dst)
+                {
+                    dst = curr_d;
+                    vertex = item;
+                }
+            }
+
+            // cv::Moments m = cv::moments(approx_curve, false);
+            // cv::Point center(m.m10/m.m00, m.m01/m.m00);
+            // cv::Vec4f line;
+            // cv::fitLine(approx_curve, line, cv::DIST_L2, 0, 0.01, 0.01);
+            // cv::line(warpedFrame, cv::Point(line[2], line[3]), cv::Point(line[2]+line(0)*80, line(3)+line(1)*80), (0,255,0), 2);
+
+
+            //cv::line(contours_img, center*scale, vertex*scale, (0,255,0), 2);
+            //cv::circle(contours_img, center*scale, 20, cv::Scalar(0,0,0), -1);
+
+            double dx = cx-vertex.x;
+            double dy = cy-vertex.y;
+
+            x = cx;
+            y = cy;
+            theta = std::atan2(dy, dx);
+
+
+            //covariance = {};
+
+            //std::cout << xc_m << " " << yc_m << " " << theta*180/M_PI << std::endl;
+        }
+
+        // cv::imshow("Original", contours_img);
+        // cv::waitKey(1);
+
+        return found;
+    }
+
+    /*!
      * Process the image to detect the robot position
      * @param[in]  image_in       input image
      * @param[in]  scale          1px/scale = X meters
@@ -18,121 +120,13 @@ namespace student {
      * @param[out] theta          yaw of the robot in the arena reference system
      * @param[in]  config_folder  A custom string from config file.
     */
-    bool findRobot(const cv::Mat &imgIn, const double scale, Polygon &triangle, double &x, double &y, double &theta,
-                   const string &config_folder) {
+    bool findRobot(const cv::Mat& img_in, const double scale, Polygon& triangle, double& x, double& y, double& theta, const std::string& config_folder){
+
+        // Convert color space from BGR to HSV
         cv::Mat hsv_img;
-        convertColorsFromRGBToHSV(imgIn, hsv_img);
-        vector<vector<cv::Point>> contours = findBlueContours(hsv_img);
-
-#ifdef FIND_ROBOT_DEBUG_PLOT // do this only if FIND_DEBUG_PLOT is defined
-        cv::imshow("findRobotHsv", hsv_img);
-        cv::imshow("findRobotMask", blue_mask);
-        cv::Mat contours_img;
-        contours_img = img_in.clone();
-        cv::drawContours(contours_img, contours, -1, cv::Scalar(0, 0, 0), 4, cv::LINE_AA);
-        cout << "N. contours: " << contours.size() << endl;
-#endif
-
-        vector<cv::Point> approx_curve;
-        vector<vector<cv::Point>> contours_approx;
-        bool found = false;
-        for (int i = 0; i < contours.size() && !found; ++i) {
-            // Approximate the i-th contours
-            cv::approxPolyDP(contours[i], approx_curve, 30, true);
-
-            // Check the number of edge of the approximate contour
-            if (approx_curve.size() == 3) {
-                double area = cv::contourArea(approx_curve);
-                found = true;
-#ifdef FIND_ROBOT_DEBUG_PLOT   // do this only if FIND_DEBUG_PLOT is defined
-                cout << (i + 1) << ") Contour size: " << contours[i].size() << endl;
-            cout << (i + 1) << ") Aprox Contour size: " << approx_curve.size() << endl;
-            cout << "Area: " << area << endl;
-            contours_approx = {approx_curve};
-
-            cv::drawContours(contours_img, contours_approx, -1, cv::Scalar(0, 0, 255), 3, cv::LINE_AA);
-#endif
-            }
-        }
-
-        // If we found the robot triangle, set robot position and create the triangle polygon
-        if (found) {
-            // emplace back every vertex on triangle (output of this function)
-            for (const auto &pt: approx_curve) {
-                triangle.emplace_back(pt.x / scale, pt.y / scale);
-                // remember to use the scale to convert the position on the image
-                // (pixels) to the position in the arena (meters)
-            }
-
-            // Find the position of the robot
-            // NB: the position of the robot coincide with the baricenter of the triangle
-            double cx = 0, cy = 0;
-
-            // Compute the triangle baricenter
-            for (auto vertex: triangle) {
-                // NB: triangle point are expressed in meters
-                cx += vertex.x;
-                cy += vertex.y;
-            }
-            cx /= static_cast<double>(triangle.size());
-            cy /= static_cast<double>(triangle.size());
-
-            // Find the robot orientation (i.e the angle of height relative to the base with the x axis)
-            double dst = 0;
-            Point top_vertex; //
-            for (auto &vertex: triangle) {
-                const double dx = vertex.x - cx;
-                const double dy = vertex.y - cy;
-                const double curr_d = dx * dx + dy * dy;
-                if (curr_d > dst) {
-                    dst = curr_d;
-                    top_vertex = vertex;
-                }
-            }
-
-            // Store the position of the robot in the output
-            x = cx;
-            y = cy;
-
-            // Compute the robot orientation
-            const double dx = cx - top_vertex.x;
-            const double dy = cy - top_vertex.y;
-            theta = atan2(dy, dx);
-
-#ifdef FIND_ROBOT_DEBUG_PLOT   // do this only if FIND_DEBUG_PLOT is defined
-            // Draw over the imag ethe ba
-            cv::Point cv_baricenter(x * scale, y * scale); // convert back m to px
-            cv::Point cv_vertex(top_vertex.x * scale, top_vertex.y * scale); // convert back m to px
-            cv::line(contours_img, cv_baricenter, cv_vertex, cv::Scalar(0, 255, 0), 3);
-            cv::circle(contours_img, cv_baricenter, 5, cv::Scalar(0, 0, 255), -1);
-            cv::circle(contours_img, cv_vertex, 5, cv::Scalar(0, 255, 0), -1);
-            cout << "(x, y, theta) = " << x << ", " << y << ", " << theta * 180 / M_PI << endl;
-#endif
-        }
-
-#ifdef FIND_ROBOT_DEBUG_PLOT   // do this only if FIND_DEBUG_PLOT is defined
-        cv::imshow("findRobot", contours_img);
-        cv::waitKey(1);
-#endif
-        return found;
-    }
-
-    void convertColorsFromRGBToHSV(const cv::Mat &img_in, const cv::Mat &hsv_img) {
         cv::cvtColor(img_in, hsv_img, cv::COLOR_BGR2HSV);
-    }
-
-    vector<vector<cv::Point>> findBlueContours(const cv::Mat &hsv_img) {// Extract blue color region
-        cv::Mat blue_mask;
-        cv::inRange(hsv_img, cv::Scalar(100, 120, 150), cv::Scalar(135, 255, 255), blue_mask);
-
-        // BLUE MASK on real world imgs contains noise
-        // to get rid of the noise a possible solution is to use:
-        // cv::erode and cv::dilate (https://docs.opencv.org/3.3.1/db/df6/tutorial_erosion_dilatation.html)
-
-        // Find blue mask contours
-        vector<vector<cv::Point>> contours;
-        cv::findContours(blue_mask, contours, cv::RETR_EXTERNAL, cv::CHAIN_APPROX_SIMPLE);
-        return contours;
+        cout << "process robot" << endl;
+        return processRobot(hsv_img, scale, triangle, x, y, theta);
     }
 
 
