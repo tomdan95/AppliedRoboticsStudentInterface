08c0afef506605febc7402ba21fe37f6666c97fc
simonedegiacomi <simonedegiacomi97@gmail.com>
2020-01-15 13:47:36 +0100

    prone path only if the pruning doesn't add collision

--- src/pipeline/planning/MissionSolver.cpp
+++ src/pipeline/planning/MissionSolver.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 using namespace student;
 
-bool canSkip(vector<Point *> toReach, Point* p) {
+bool canSkip(vector<Point *> toReach, Point *p) {
     for (auto &i : toReach) {
         if (i == p) {
             return false;
@@ -14,26 +14,36 @@ bool canSkip(vector<Point *> toReach, Point* p) {
 
 
 void MissionSolver::prunePath(vector<Point *> *path, vector<Point *> toReach, double threshold) {
-    cout << "PATH = " << path->size() << endl;
-    auto it = path->begin();
+    cout << "[PATH-PRUNING] Original path nodes = " << path->size() << endl;
+    auto it = path->begin() + 1;
     while (it + 1 != path->end()) {
-        Point* p = *it;
-        Point* next = *(it + 1);
+        auto previous = it - 1;
+        auto p = it;
+        auto next = it + 1;
 
-        // TODO: This may add collisions, that then doublin can't fix!
-        // TODO: maybe, don't compute euclidian distances but path distances
-        double distance = Graph::distanceBetween(*p, *next);
-        cout << distance << endl;
+        double distance = Graph::distanceBetween(**p, **next);
         bool isShort = distance < threshold;
-        if (isShort && canSkip(toReach, p) && !collisionDetector->doesSegmentCollide(**(it - 1), *next)) {
+        bool removed = false;
+        if (isShort && canSkip(toReach, *p) && !collisionDetector->doesSegmentCollide(**previous, **next)) {
+            // we don't need to reach point 'p', and segment from 'previous' to 'next' doesn't have
+            // collisions, so we can remove p
             it = path->erase(it);
-        } else if (isShort && canSkip(toReach, next)) { // TODO: Check collision also here
-            path->erase(it + 1);
-        } else {
+            removed = true;
+        } else if (isShort && canSkip(toReach, *next)) {
+            auto nextNext = next + 1;
+            if (nextNext != path->end() && !collisionDetector->doesSegmentCollide(**p, **nextNext)) {
+                // we don't need to reach the next point, and segment from point 'p' to the successor of next
+                // doesn't have collisions, so we ca remove it
+                path->erase(next);
+                removed = true;
+            }
+        }
+        if (!removed) {
             it++;
         }
+
     }
-    cout << "PATH AFTER = " << path->size() << endl;
+    cout << "[PATH-PRUNING] PATH POINTS AFTER = " << path->size() << endl;
 }
 
 
